bc2901c527885e4533bcd1a1d9ebd781faed7bdc
J <jpham26@gmail.com>
2024-01-30 14:26:12 -0800

    Clean up and refactor player.js

--- src/components/player.js
+++ src/components/player.js
@@ -1,8 +1,12 @@
 // player.js
-import { generateRandomCoord, generateSetOfCoords, isOutOfBounds } from "./utils.js";
+import { 
+  generateRandomCoord, 
+  generateSetOfCoords, 
+  isOutOfBounds 
+} from "./utils.js";
 
+// Factory for creating human player
 export const createPlayer = (name, isHuman) => {
-
   const attack = (opponentGameBoard, chosenMove = null) => {
     let move = chosenMove;        
     const attackResult = opponentGameBoard.receiveAttack(move);
@@ -12,16 +16,20 @@ export const createPlayer = (name, isHuman) => {
   return { name, isHuman, attack };
 };
 
+// Factory for creating computer player
 export const createComputerPlayer = (name, isHuman) => {
-  // Store coordinates to keep track of possible AI moves
+
+  // Store coordinates to keep track of possible computer moves
   let possibleMoves = generateSetOfCoords();
   let lastHit = null;
 
   const updatePossibleMoves = (opponentGameBoard) => {
     possibleMoves = new Set();
+
     for (let row = 0; row < 10; row++) {
       for (let col = 0; col < 10; col++) {
-        if (opponentGameBoard.board[row][col] !== 'hit' && opponentGameBoard.board[row][col] !== 'miss') {
+        if (opponentGameBoard.board[row][col] !== 'hit' && 
+            opponentGameBoard.board[row][col] !== 'miss') {
           possibleMoves.add([row, col]);
         }
       }
@@ -29,7 +37,6 @@ export const createComputerPlayer = (name, isHuman) => {
   }
 
   const updateAttackState = (attackResult, move) => {
-    
     if (attackResult === true) {
       lastHit = move;
     } else {
@@ -44,7 +51,6 @@ export const createComputerPlayer = (name, isHuman) => {
    
     if (lastHit) {
       move = getNextAdjacentTarget(lastHit, possibleMoves, opponentGameBoard);
-      console.log(`Attack: ${move}`)
     } else {
       move = generateRandomCoord(possibleMoves);
     }
@@ -58,8 +64,8 @@ export const createComputerPlayer = (name, isHuman) => {
   return { name, isHuman, attack };
 };
 
-// Helper functions
 
+// Helper function to choose the next computer move after a hit
 const getNextAdjacentTarget = (lastHit, possibleMoves, opponentGameBoard) => {
   const nextPossibleMoves = [
     [lastHit[0] - 1, lastHit[1]],
@@ -68,9 +74,10 @@ const getNextAdjacentTarget = (lastHit, possibleMoves, opponentGameBoard) => {
     [lastHit[0], lastHit[1] + 1],
   ];
 
+  // Convert the Set to an Array for looping
   const possibleMovesArray = Array.from(possibleMoves)
 
-  // Filter out moves not in possibleMoves
+  // Filter out moves that are not in possibleMoves
   const validMoves = nextPossibleMoves.filter(move =>
     !isOutOfBounds(move[0], move[1]) && 
     possibleMovesArray.some(possibleMove => 
@@ -80,12 +87,14 @@ const getNextAdjacentTarget = (lastHit, possibleMoves, opponentGameBoard) => {
 
   // Keep track until a valid move is found
   while (validMoves.length > 0) {
+    // Choose a random move from validMoves
     const index = Math.floor(Math.random() * validMoves.length);
     const move = validMoves[index];
-    console.log(`Move: ${move}`)
   
-    // Check chosen move is valid and not already hit or miss and is within the boundary of board
-    if (opponentGameBoard.board[move[0]][move[1]] !== 'hit' && opponentGameBoard.board[move[0]][move[1]] !== 'miss') {
+    // Check if chosen move is valid and cell has not already 
+    // been targeted, and is within the boundary of the board
+    if (opponentGameBoard.board[move[0]][move[1]] !== 'hit' && 
+        opponentGameBoard.board[move[0]][move[1]] !== 'miss') {
       return move;
     } else {
       validMoves.splice(index, 1);
